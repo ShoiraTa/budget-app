bcd59e5b2f910e219db7b1b7fc614d657ef16ee9
shoiraTa <shoira.shakirovna@gmail.com>
2022-03-23 09:32:22 +0400

    Add login sign up pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  protect_from_forgery prepend: true
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name])
+  end
 end
